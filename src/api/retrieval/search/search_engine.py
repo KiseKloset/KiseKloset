c2c7909e8169783694f47996a99274713fdd9aac
TÙNG PHAN NGUYỄN THANH <19120424@student.hcmus.edu.vn>
2023-07-10 22:40:16 +0700

    Add approximate searching

---
+++ src/api/retrieval/search/search_engine.py
@@ -0,0 +1,42 @@
+import faiss
+from .faiss_index_factory import FaissIndexFactory
+import numpy as np
+
+
+class SearchEngine:
+    def __init__(self, indices, features, index = None):
+        super().__init__()
+        self.indices = indices
+        if index is None:
+            self.index = FaissIndexFactory.create_index(features, "hnsw_32_32_128")
+            self.index.add(features)
+        else:
+            self.index = index
+
+
+    def run(self, query, k = 1):
+        _, I = self.index.search(query.unsqueeze(0).cpu().detach().numpy(), k)        
+        return [self.indices[i] for i in I[0] if i >= 0]
+    
+
+    def save(search_engine, dir, name):
+        indices_path = dir / (name + ".npy")
+        np.save(str(indices_path), search_engine.indices)
+        index_path = dir / (name + ".index") 
+        faiss.write_index(search_engine.index, str(index_path))
+
+
+    def load(dir, name):
+        indices_path = dir / (name + ".npy")
+        index_path = dir / (name + ".index") 
+        indices = np.load(str(indices_path))
+        return SearchEngine(indices, None, faiss.read_index(str(index_path)))  
+
+
+    def can_load(dir, name):
+        indices_path = dir / (name + ".npy")
+        index_path = dir / (name + ".index") 
+        return indices_path.exists() and index_path.exists()
+    
+
+    
